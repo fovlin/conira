957a8adebcbb8fde49da63527113439454fc6e71
fovlin <fovlin@163.com>
2026-09-09 15:27:07 +0800

    add log in feature

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ type Config struct {
 }
 
 var (
+    loginFile = "login.html"
     accountsFile = "accounts.json"
     indexFile = "index.html"
     itemFile = "item.html"
@@ -60,9 +61,11 @@ func loadUser() (map[string]User, error) {
 
     file, err := os.Open(accountsFile)
     if os.IsNotExist(err) {
+        fmt.Println(err)
         return nil, nil
     }
     if err != nil {
+        fmt.Println(err)
         return nil, err
     }
 
@@ -89,17 +92,77 @@ func saveMessages(msgs []Message) error {
     return encoder.Encode(msgs)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    indexData, err := os.ReadFile(indexFile)
+func login(w http.ResponseWriter) {
+    loginFile, err := os.ReadFile(loginFile)
     if err != nil {
         fmt.Println("404 Not Found: " + indexFile)
         os.Exit(1)
+    }
+    w.Write(loginFile)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.Method != http.MethodPost {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    userName := r.FormValue("user-name")
+    password := r.FormValue("password")
+    if userName == "" || password == "" {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    userList, err:= loadUser()
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    user, ok := userList[userName]
+
+    if !ok {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    if user.Password != password {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    cookie := &http.Cookie{
+        Name: "IsLogin",
+        Value: user.UserName,
+        Expires: time.Now().Add(time.Hour * 24 * 3),
+    }
+
+    http.SetCookie(w, cookie)
+    http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+
+    cookie, err := r.Cookie("IsLogin")
+    if err == http.ErrNoCookie {
+        login(w)
+        return
+    } else if err != nil {
+        fmt.Println(err)
+    }
+
+    indexData, err := os.ReadFile(indexFile)
+    if err != nil {
+        fmt.Println("404 Not Found: " + indexFile)
     }
 
     itemData, err := os.ReadFile(itemFile)
     if err != nil {
         fmt.Println("404 Not Found: " + itemFile)
-        os.Exit(1)
     }
 
     MessageList, err := loadMessages()
@@ -122,6 +185,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
         allMessage = []byte("<p style=\"margin: auto auto\">None any message</p>")
     }
 
+    userName := []byte(cookie.Value)
+    indexData = bytes.ReplaceAll(indexData, []byte("{{ user-name }}"), userName)
     indexData = bytes.ReplaceAll(indexData, []byte("{{ messages }}"), allMessage)
     w.Write(indexData)
 
@@ -133,9 +198,16 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    name := r.FormValue("name")
+    cookie, err := r.Cookie("IsLogin")
+    if err != nil {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    name := cookie.Value
+
     content := r.FormValue("content")
-    if name == "" || content == "" {
+    if content == "" {
         http.Redirect(w, r, "/", http.StatusSeeOther)
         return
     }
@@ -157,37 +229,46 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-    var config Config
-
-    configFile, err := os.Open(configFile)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    defer configFile.Close()
-
-    err = json.NewDecoder(configFile).Decode(&config)
+    config, err := loadConfig()
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
     
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/add-note", addHandler)
+    http.HandleFunc("/", mainHandler)
+    http.HandleFunc("/login", loginHandler)
+    http.HandleFunc("/commit", addHandler)
 
     if config.TLS {
         fmt.Println("server start on: https://" + config.Ip + ":" + fmt.Sprint(int(config.Port)))
         if err := http.ListenAndServeTLS(config.Ip + ":" + fmt.Sprint(int(config.Port)), config.TLS_CRT, config.TLS_KEY, nil); err != nil {
             fmt.Println(err)
             os.Exit(1)
-        } else {
         }
     } else {
         fmt.Println("server start on: http://" + config.Ip + ":" + fmt.Sprint(int(config.Port)))
         if err := http.ListenAndServe(config.Ip + ":" + fmt.Sprint(int(config.Port)), nil); err != nil {
             fmt.Println(err)
             os.Exit(1)
-        } else {
         }
     }
+}
+
+func loadConfig() (Config, error) {
+
+    var config Config
+
+    configFile, err := os.Open(configFile)
+    if err != nil {
+        return Config{}, err
+    }
+    defer configFile.Close()
+
+    err = json.NewDecoder(configFile).Decode(&config)
+    if err != nil {
+        return Config{}, err
+    }
+
+    return config, nil
+
 }
